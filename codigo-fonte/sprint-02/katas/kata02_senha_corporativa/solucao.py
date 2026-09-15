c2a7b550b101020e9b31806307d237c7a1e2ea84
BernardoApl <bblopinho123@gmail.com>
2026-09-15 17:15:13 -0300

    Implementacao da  kata 2 #35

--- codigo-fonte/sprint-02/katas/kata02_senha_corporativa/solucao.py
+++ codigo-fonte/sprint-02/katas/kata02_senha_corporativa/solucao.py
@@ -1,5 +1,25 @@
-"""Kata 02: Validador de Senha Corporativa. Implemente a função abaixo."""
+"""Kata 02: calculo de estacionamento."""
 
 
-def validar_senha(senha: str, nome_usuario: str) -> bool:
-    raise NotImplementedError
+def validar_senha(horas, tipo_veiculo):
+    if horas <= 0:
+        raise ValueError("A quantidade de horas deve ser maior que zero.")
+
+    tipo_veiculo = tipo_veiculo.lower()
+    if tipo_veiculo not in ["carro", "moto"]:
+        raise ValueError("Tipo de veiculo invalido. Use 'carro' ou 'moto'.")
+
+    if horas <= 1:
+        valor_base = 6.00
+    else:
+        valor_base = 6.00 + (horas - 1) * 4.00
+
+    if horas > 8:
+        valor_base = 35.00
+
+    if tipo_veiculo == "moto":
+        valor_final = valor_base * 0.80
+    else:
+        valor_final = valor_base
+
+    return float(valor_final)
